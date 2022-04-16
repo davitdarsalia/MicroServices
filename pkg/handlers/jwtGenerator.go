562604f5ea5eaa320cb26a5b1fd542df8d252aac
davitdarsalia <darsalia.david1998@gmail.com>
2022-04-17 00:08:59 +0400

    feat: Additional RefreshLogin Handler

--- pkg/handlers/jwtGenerator.go
+++ pkg/handlers/jwtGenerator.go
@@ -8,32 +8,22 @@ import (
 	"time"
 )
 
-type claims struct {
-	jwt.StandardClaims
-	userId int
-}
-
 func JwtGenerator(userId string) string {
 	var secretKey = []byte(os.Getenv("SIGN_KEY"))
 	expiryDate, parseIntErr := strconv.ParseInt(os.Getenv("JWT_EXPIRY_DATE"), 0, 0)
-
 	if parseIntErr != nil {
 		log.Println(parseIntErr)
 	}
-
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+	jwtClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		ExpiresAt: time.Now().Add(time.Minute * time.Duration(expiryDate)).Unix(),
 		Id:        userId,
 		IssuedAt:  time.Now().Unix(),
 		Subject:   "Authentication Regular User",
 	})
-
-	token, err := claims.SignedString(secretKey)
-
+	token, err := jwtClaims.SignedString(secretKey)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return token
 }
 
@@ -42,10 +32,19 @@ func TokenIsValid(tokenStr string) bool {
 	token, tokenValidationErr := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
 		return []byte(key), nil
 	})
-
 	if tokenValidationErr != nil {
 		return false
 	}
-
 	return token.Valid
+}
+
+func RefreshLogin(token string, userId string) (string, error) {
+	valid := TokenIsValid(token)
+
+	if valid {
+		newToken := JwtGenerator(userId)
+		return newToken, nil
+	}
+	newToken := JwtGenerator(userId)
+	return newToken, nil
 }
