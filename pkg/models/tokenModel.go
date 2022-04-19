f9592f493c879d03580dba7e2be4284a32af639d
davitdarsalia <darsalia.david1998@gmail.com>
2022-04-19 14:06:56 +0400

    feat: Token Model Modified

--- pkg/models/tokenModel.go
+++ pkg/models/tokenModel.go
@@ -1,8 +1,11 @@
 package models
 
+import "time"
+
 type TokenModel struct {
-	AccessToken string `json:"access_token"`
-	ExpiresIn   int64  `json:"expires_in"`
-	TokenType   string `json:"token_type"`
-	UserId      string `json:"user_id"`
+	AccessToken  string        `json:"access_token"`
+	RefreshToken string        `json:"refresh_token"`
+	ExpiresIn    time.Duration `json:"expires_in"`
+	TokenType    string        `json:"token_type"`
+	UserId       string        `json:"user_id"`
 }
