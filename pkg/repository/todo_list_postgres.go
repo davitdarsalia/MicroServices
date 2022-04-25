435b395ec15c6c45b5cf0dbdb8282496353aab32
davitdarsalia <darsalia.david1998@gmail.com>
2022-04-26 01:06:04 +0400

    feat: Todo Model + TodoPostgresMethods Defined

---
+++ pkg/repository/todo_list_postgres.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"fmt"
+	todo "github.com/davitdarsalia/BookStoreMicroservices"
+	"github.com/jmoiron/sqlx"
+)
+
+type TodoListPostgres struct {
+	db *sqlx.DB
+}
+
+func NewTodoListPostgres(db *sqlx.DB) *TodoListPostgres {
+	return &TodoListPostgres{db: db}
+}
+
+func (r *TodoListPostgres) Create(userId int, list todo.TodoList) (int, error) {
+	tx, err := r.db.Begin()
+	if err != nil {
+		return 0, err
+	}
+
+	var id int
+	createListQuery := fmt.Sprintf("INSERT INTO %s (title,description) VALUES($1, $2) RETURNING id", todoListsTable)
+	row := tx.QueryRow(createListQuery, list.Title, list.Description)
+	if err = row.Scan(&id); err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	createUsersListQuery := fmt.Sprintf("INSERT INTO %s (user_id, list_id) VALUES ($1, $2)", usersListsTable)
+	_, err = tx.Exec(createUsersListQuery, userId, id)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	return id, tx.Commit()
+}
