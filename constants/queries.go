987b33fe180e1f0650ad81eee9da5f1d5f9e2ee8
DavitDarsalia1 <darsalia.davit1998@gmail.com>
2022-05-14 18:48:05 +0400

    Changes in queries

--- constants/queries.go
+++ constants/queries.go
@@ -1,6 +1,6 @@
 package constants
 
 const (
-	RegisterUserQuery = `INSERT INTO "public.User" (personal_number, phonenum, username, email, firstname, lastname, ip_address, password, salt) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING userid`
-	CheckUserQuery    = `SELECT userid FROM "public.User WHERE username=$1 AND password=$2"`
+	RegisterUserQuery = `INSERT INTO users (personal_number, phonenum, username, email, firstname, lastname, ip_address, password, salt) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING userid`
+	CheckUserQuery    = `SELECT userid FROM users WHERE username=$1 AND password=$2`
 )
