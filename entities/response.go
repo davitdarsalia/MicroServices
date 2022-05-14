bae1788e763234ff9bfc8651f11a26fea1d4c3ae
DavitDarsalia1 <darsalia.davit1998@gmail.com>
2022-05-14 20:03:03 +0400

    feat: Additional Response Added

--- entities/response.go
+++ entities/response.go
@@ -5,3 +5,9 @@ type RegisteredUserResponse struct {
 	Message   string `json:"message"`
 	CreatedAt string `json:"created_at"`
 }
+
+type SignedInUserResponse struct {
+	UserId      int    `json:"user_id"`
+	Message     string `json:"message"`
+	AccessToken string `json:"access_token"`
+}
