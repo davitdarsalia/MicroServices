1128f876ab899bd9e900e5679956f4e1d19de744
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-17 20:29:32 +0400

    bugfix: Reset password query bugfix

--- constants/queries.go
+++ constants/queries.go
@@ -3,5 +3,5 @@ package constants
 const (
 	RegisterUserQuery = `INSERT INTO users (personal_number, phonenum, username, email, firstname, lastname, ip_address, password, salt) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING userid`
 	CheckUserQuery    = `SELECT userid FROM users WHERE username=$1 AND password=$2`
-	CheckUserByEmail  = `SELECT userid FROM users WHERE email=$1 username=$2 personal_number=$3 `
+	CheckUserByEmail  = `SELECT userid FROM users WHERE email=$1 and username=$2 and personal_number=$3 `
 )
