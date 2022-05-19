adfd693dad02ca861369c38c1bd9db5d112970f9
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-19 16:15:27 +0400

    feat: GlobalHelper JwtGenerator Defined

---
+++ entities/jwtGenerator.go
@@ -0,0 +1,39 @@
+package entities
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net"
+	"os"
+	"time"
+)
+
+func GenerateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, CustomToken{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 2).Unix(),
+			Id:        fmt.Sprintf("%d", userID),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    os.Getenv("ISSUER"),
+			Subject:   "Authentication",
+		},
+		UserID: userID,
+		Role:   "user",
+		Ip:     GetIp(),
+	})
+	return token.SignedString([]byte(SignKey))
+}
+
+func GetIp() string {
+	var result string
+
+	host, _ := os.Hostname()
+	address, _ := net.LookupIP(host)
+	for _, a := range address {
+		if ipv4 := a.To4(); ipv4 != nil {
+			result = fmt.Sprintf("IPv4: %s ", ipv4)
+		}
+	}
+
+	return result
+}
