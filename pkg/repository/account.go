df860e3aa3832b1f92f61b2fd7b2858ef63f6767
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-19 22:07:50 +0400

    feat: Interface Contracts <Demo>

--- pkg/repository/account.go
+++ pkg/repository/account.go
@@ -1,1 +1,45 @@
 package repository
+
+func (r *AuthPostgres) GetUserInfo() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) GetTrustedDevices() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) GetUserById() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) BlockUser() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) UnblockUser() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) BlockedUsersList() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) UploadProfileImage() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) LogoutSession() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) UpdateProfileDetails() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) UpdateTrustedDevices() {
+	//TODO implement me
+}
+
+func (r *AuthPostgres) SetPasscode() {
+	//TODO implement me
+}
