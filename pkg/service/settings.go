d450e18526e9c83fb8681b836202625197fbc851
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-19 23:04:45 +0400

    feat: Settings Services Defined On Repo And Service Level

---
+++ pkg/service/settings.go
@@ -0,0 +1,37 @@
+package service
+
+func (s SettingsService) GetProfileSettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) GetNotificationSettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) GetPaymentOptions() {
+	//TODO implement me
+}
+
+func (s SettingsService) GetPrivacySettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) GetSecuritySettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) UpdateNotificationSettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) UpdatePaymentOptions() {
+	//TODO implement me
+}
+
+func (s SettingsService) UpdatePrivacySettings() {
+	//TODO implement me
+}
+
+func (s SettingsService) UpdateSecuritySettings() {
+	//TODO implement me
+}
