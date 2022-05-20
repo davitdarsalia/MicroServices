bc0a852336e2f07cbe502a8b5f276cea29f2403b
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-20 23:39:35 +0400

    feat: UserInfo Table Query Instance

--- sqlInstances/queries.sql
+++ sqlInstances/queries.sql
@@ -4,4 +4,16 @@ UPDATE profileactivities set
             ),
             last_reset_password = '1998-08-08',
             last_password_change = '1998-08-08',
-            location = 'Tbilisi'
+            location = 'Tbilisi';
+
+CREATE TABLE userinfo (
+            profileImage  bytea,
+            followers INT DEFAULT 0 NOT NULL,
+            following INT DEFAULT 0 NOT NULL,
+            blocked_users_amount INT DEFAULT 0 NOT NULL,
+            working_place VARCHAR(200),
+            education VARCHAR(200),
+            origin VARCHAR(200),
+            additional_email VARCHAR(200),
+            userid BIGINT REFERENCES users (userid)
+)
