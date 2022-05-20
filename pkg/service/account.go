242c7095564e7674d750e5873096a25859c144a0
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-20 23:41:30 +0400

    feat: GetAccountInfo Service

--- pkg/service/account.go
+++ pkg/service/account.go
@@ -1,49 +1,62 @@
 package service
 
-func (a AccountService) GetProfileDetails() {
+import (
+	"github.com/davitdarsalia/LendAppBackend/entities"
+	"log"
+	"strconv"
+)
+
+func (a *AccountService) GetProfileDetails() (*entities.ProfileDetails, error) {
+	id, _ := a.redisConn.Get(localContext, "UserID").Result()
+	intID, err := strconv.Atoi(id)
+
+	if err != nil {
+		log.Println("[Account Service] - ParseInt Handler Error")
+	}
+
+	return a.repo.GetProfileDetails(intID)
+}
+
+func (a *AccountService) GetUserInfo() {
 	//TODO implement me
 }
 
-func (a AccountService) GetUserInfo() {
+func (a *AccountService) GetTrustedDevices() {
 	//TODO implement me
 }
 
-func (a AccountService) GetTrustedDevices() {
+func (a *AccountService) GetUserById() {
 	//TODO implement me
 }
 
-func (a AccountService) GetUserById() {
+func (a *AccountService) BlockUser() {
 	//TODO implement me
 }
 
-func (a AccountService) BlockUser() {
+func (a *AccountService) UnblockUser() {
 	//TODO implement me
 }
 
-func (a AccountService) UnblockUser() {
+func (a *AccountService) BlockedUsersList() {
 	//TODO implement me
 }
 
-func (a AccountService) BlockedUsersList() {
+func (a *AccountService) UploadProfileImage() {
 	//TODO implement me
 }
 
-func (a AccountService) UploadProfileImage() {
+func (a *AccountService) LogoutSession() {
 	//TODO implement me
 }
 
-func (a AccountService) LogoutSession() {
+func (a *AccountService) UpdateProfileDetails() {
 	//TODO implement me
 }
 
-func (a AccountService) UpdateProfileDetails() {
+func (a *AccountService) UpdateTrustedDevices() {
 	//TODO implement me
 }
 
-func (a AccountService) UpdateTrustedDevices() {
-	//TODO implement me
-}
-
-func (a AccountService) SetPasscode() {
+func (a *AccountService) SetPasscode() {
 	//TODO implement me
 }
