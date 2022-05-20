505fd17e6d2bfc6bafc5e091d05aa55a6096e520
davitdarsalia <darsalia.david1998@gmail.com>
2022-05-20 16:00:08 +0400

    feat: Handlers Defined

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -27,7 +27,31 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	{
 		account := protected.Group("/account", h.checkAuth)
 		{
-			account.GET("/user-info", h.signUp)
+			account.GET("/profile-details", h.GetProfileDetails)
+			account.GET("/user-info", h.GetUserInfo)
+			account.GET("/trusted-devices-list", h.GetTrustedDevices)
+			account.GET("/get-user-by-id/{id}", h.GetUserById)
+			account.POST("/block-user", h.BlockUser)
+			account.GET("/unblock-user", h.UnblockUser)
+			account.GET("/blocked-user-list", h.BlockedUsersList)
+			account.GET("/upload-profile-image", h.UploadProfileImage)
+			account.GET("/logout", h.LogoutSession)
+			account.GET("/update-profile-details", h.UpdateProfileDetails)
+			account.GET("/update-trusted-devices", h.UpdateTrustedDevices)
+			account.GET("/set-passcode", h.SetPasscode)
+		}
+
+		settings := protected.Group("/settings", h.checkAuth)
+		{
+			settings.GET("/profile-settings", h.GetProfileSettings)
+			settings.GET("/notification-settings", h.GetNotificationSettings)
+			settings.GET("/payment-options", h.GetPaymentOptions)
+			settings.GET("/privacy-settings", h.GetPrivacySettings)
+			settings.GET("/security-settings", h.GetSecuritySettings)
+			settings.GET("/update-notification-settings", h.UpdateNotificationSettings)
+			settings.GET("/update-payment-options", h.UpdatePaymentOptions)
+			settings.GET("/update-privacy-settings", h.UpdatePrivacySettings)
+			settings.GET("/update-security-settings", h.UpdateSecuritySettings)
 		}
 
 	}
