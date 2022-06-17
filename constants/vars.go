b95986626b56f79ca8249af73c0524587fa8eab0
davitdarsalia <darsalia.david1998@gmail.com>
2022-06-17 23:18:34 +0400

    feat: Consts defined

--- constants/vars.go
+++ constants/vars.go
@@ -1,6 +1,9 @@
 package constants
 
-var (
-	RedisID   = "UserID"
-	SessionID = "SessionID"
+const (
+	RedisID        = "UserID"
+	SessionID      = "SessionID"
+	TokenBlackList = "TokenBlackList"
+	FakeToken      = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTQ5Njg3MjgsImp0aSI6IjM0IiwiaWF0IjoxNjU0OTYwOTI4LCJpc3MiOiJEX0JhbmsiLCJzdWIiOiJBdXRoZW50aWNhdGlvbiIsInVzZXJfaWQiOjM0LCJ1c2VybmFtZSI6IiIsInJvbGUiOiJ1c2VyIiwiaXBfYWRkcmVzcyI6IklQdjQ6IDE3Mi4yMC45Ni45MSAifQ.xvgWX_TBdyhbWOKWESr1Pjjg0LIS8Vyr-UIkhhxBCIU"
+	NotValidMark   = " [Not Valid Session ID]"
 )
