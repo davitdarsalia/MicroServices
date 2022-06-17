85dba3299c3cb9e8f1c02bf3ee88615bbca66b24
davitdarsalia <darsalia.david1998@gmail.com>
2022-06-17 23:16:55 +0400

    feat: Enabling GoRoutines For Every Request

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -14,33 +14,33 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 	auth := r.Group("/api/auth")
 	{
-		auth.POST("/sign-up", h.signUp)
-		auth.POST("/sign-in", h.signIn)
-		auth.POST("/reset-password", h.resetPassword)
-		auth.POST("/verify-reset-email", h.validateResetEmail)
+		go auth.POST("/sign-up", h.signUp)
+		go auth.POST("/sign-in", h.signIn)
+		go auth.POST("/reset-password", h.resetPassword)
+		go auth.POST("/verify-reset-email", h.validateResetEmail)
 
-		auth.POST("/reset-password-profile", h.resetPasswordProfile, h.checkAuth)
-		auth.POST("/refresh-login", h.refreshLogin, h.checkAuth)
+		go auth.POST("/reset-password-profile", h.resetPasswordProfile, h.checkAuth)
+		go auth.POST("/refresh-login", h.refreshLogin, h.checkAuth)
 	}
 
 	protected := r.Group("api/protected")
 	{
 		account := protected.Group("/account", h.checkAuth)
 		{
-			account.GET("/profile-details", h.GetProfileDetails)
-			account.GET("/user-info", h.GetUserInfo)
-			account.GET("/trusted-devices-list", h.GetTrustedDevices)
-			account.PUT("/add-trusted-device", h.AddTrustedDevice)
-			account.POST("/block-user", h.BlockUser)
-			account.POST("/unblock-user", h.UnblockUser)
-			account.GET("/blocked-user-list", h.BlockedUsersList)
-			account.POST("/upload-profile-image", h.UploadProfileImage)
-			account.POST("/logout", h.LogoutSession)
-			account.PUT("/update-profile-details", h.UpdateProfileDetails)
-			account.POST("/set-passcode", h.SetPasscode)
+			go account.GET("/profile-details", h.GetProfileDetails)
+			go account.GET("/user-info", h.GetUserInfo)
+			go account.GET("/trusted-devices-list", h.GetTrustedDevices)
+			go account.PUT("/add-trusted-device", h.AddTrustedDevice)
+			go account.POST("/block-user", h.BlockUser)
+			go account.POST("/unblock-user", h.UnblockUser)
+			go account.GET("/blocked-user-list", h.BlockedUsersList)
+			go account.POST("/upload-profile-image", h.UploadProfileImage)
+			go account.POST("/logout", h.LogoutSession)
+			go account.PUT("/update-profile-details", h.UpdateProfileDetails)
+			go account.POST("/set-passcode", h.SetPasscode)
 		}
 
-		settings := protected.Group("/settings", h.checkAuth)
+		settings := protected.Group("/settings", h.checkAuth, h.SessionManager)
 		{
 			settings.GET("/profile-settings", h.GetProfileSettings)
 			settings.GET("/notification-settings", h.GetNotificationSettings)
