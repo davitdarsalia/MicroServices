b42fb40d322ee04139a48c4f0ee40f147f2143af
davitdarsalia <darsalia.david1998@gmail.com>
2022-07-08 19:55:43 +0400

    chore: Regexes Defined

--- pkg/service/regex.go
+++ pkg/service/regex.go
@@ -1,9 +1,11 @@
 package service
 
 const (
-	EmailRegex     = `\w+([-+.]\w+)*@(?!(hotmail|gmail|yahoo|msn|excite|lycos|aol|live|mail)\.com$)\w+([-.]\w+)*\.\w+([-.]\w+)*`
-	UsernameRegex  = `^[a-zA-Z0-9][a-zA-Z0-9_]{2,29}$`
-	NameRegex      = `([a-zA-Z]{1,64}\s*)+`
-	PasswordRegex  = `^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$`
-	IPAddressRegex = `^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$`
+	passwordRegex       = `^[A-Z][a-zA-Z_1234567890.,/?!@#$%^&*()";:<>,./]{8,}$`
+	nameRegex           = `[A-Z]{1,}[a-zA-Z\s]{2,}[A-Z]{1,}[a-z]{1,}[^!@#$%^&*~./;?<>"=|]`
+	emailRegex          = `[a-z_.123456789]{3,}[^!@#$%^&*()-+=|}{;:",./]@gmail.com`
+	usernameRegex       = `^[a-zA-Z0-9_@.!-#$%^&*]{8,}`
+	ipAddressRegex      = `[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}`
+	mobileNumberRegex   = `[0-9_+]{10,}[^!@#$%^&*()_+<>,.'?/:;"'[\]]`
+	personalNumberRegex = `[0-9]{10,15}`
 )
