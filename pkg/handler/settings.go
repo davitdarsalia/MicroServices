58f95fc3ba396b0d44d6b8366571ac3d243ee566
davitdarsalia <darsalia.david1998@gmail.com>
2022-07-09 20:13:33 +0400

    feat: Settings Post Method Annotations Defined

--- pkg/handler/settings.go
+++ pkg/handler/settings.go
@@ -18,9 +18,22 @@ func (h *Handler) getPaymentOptions(c *gin.Context) {
 func (h *Handler) getPrivacySettings(c *gin.Context) {
 
 }
+
 func (h *Handler) getSecuritySettings(c *gin.Context) {
 
 }
+
+// @Summary Update Notification Settings
+// @Tags Protected - Settings
+// @Description Updates Notification Settings
+// @ID update-notification-settings
+// @Accept json
+// @Produce json
+// @Param input body entities.NotificationSettings true "Credentials"
+// @Success 205 {string} constants.UpdateNotificationSettingsSuccess
+// @Failure 400 {object} localError
+// @Failure 500 default {object} localError
+// @Router /api/protected/settings/update-notification-settings  [post]
 func (h *Handler) updateNotificationSettings(c *gin.Context) {
 	var n entities.NotificationSettings
 
@@ -33,7 +46,21 @@ func (h *Handler) updateNotificationSettings(c *gin.Context) {
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, constants.UpdateNotificationSettingsError)
 	}
+
+	c.JSON(http.StatusResetContent, constants.UpdateNotificationSettingsSuccess)
 }
+
+// @Summary Update Payment Settings
+// @Tags Protected - Settings
+// @Description Updates Payment Settings
+// @ID update-payment-settings
+// @Accept json
+// @Produce json
+// @Param input body entities.PaymentSettings true "Credentials"
+// @Success 205 {string} constants.UpdatePaymentSettingsSuccess
+// @Failure 400 {object} localError
+// @Failure 500 default {object} localError
+// @Router /api/protected/settings/update-payment-settings  [post]
 func (h *Handler) updatePaymentOptions(c *gin.Context) {
 	var p entities.PaymentSettings
 
@@ -46,7 +73,21 @@ func (h *Handler) updatePaymentOptions(c *gin.Context) {
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, constants.UpdatePaymentSettingsError)
 	}
+
+	c.JSON(http.StatusResetContent, constants.UpdatePaymentSettingsSuccess)
 }
+
+// @Summary Update Security Settings
+// @Tags Protected - Settings
+// @Description Updates Security Settings
+// @ID update-security-settings
+// @Accept json
+// @Produce json
+// @Param input body entities.SecuritySettings true "Credentials"
+// @Success 205 {string} constants.UpdateSecuritySettingsSuccess
+// @Failure 400 {object} localError
+// @Failure 500 default {object} localError
+// @Router /api/protected/settings/update-security-settings  [post]
 func (h *Handler) updateSecuritySettings(c *gin.Context) {
 	var s entities.SecuritySettings
 
@@ -59,6 +100,8 @@ func (h *Handler) updateSecuritySettings(c *gin.Context) {
 	if err != nil {
 		newErrorResponse(c, http.StatusInternalServerError, constants.UpdateSecuritySettingsError)
 	}
+
+	c.JSON(http.StatusResetContent, constants.UpdateSecuritySettingsSuccess)
 }
 func (h *Handler) updatePrivacySettings(c *gin.Context) {
 
