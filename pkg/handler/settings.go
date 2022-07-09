5bdfd0bbbd662438cb69e7ecc042ccb6b82300ba
davitdarsalia <darsalia.david1998@gmail.com>
2022-07-09 15:17:21 +0400

    feat: Endpoint Methods Skeletons Defined

--- pkg/handler/settings.go
+++ pkg/handler/settings.go
@@ -1,9 +1,13 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/davitdarsalia/LendAppBackend/constants"
+	"github.com/davitdarsalia/LendAppBackend/entities"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) getProfileSettings(c *gin.Context) {
-
 }
 func (h *Handler) getNotificationSettings(c *gin.Context) {
 
@@ -18,14 +22,44 @@ func (h *Handler) getSecuritySettings(c *gin.Context) {
 
 }
 func (h *Handler) updateNotificationSettings(c *gin.Context) {
+	var n entities.NotificationSettings
 
+	if err := c.BindJSON(&n); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, constants.BadRequest)
+	}
+	// TODO - Here
+	err := h.services.Settings.UpdateNotificationSettings(&n)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, constants.UpdateNotificationSettingsError)
+	}
 }
 func (h *Handler) updatePaymentOptions(c *gin.Context) {
+	var p entities.PaymentSettings
 
-}
-func (h *Handler) updatePrivacySettings(c *gin.Context) {
+	if err := c.BindJSON(&p); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, constants.BadRequest)
+	}
+	// TODO - Here
+	err := h.services.Settings.UpdatePaymentSettings(&p)
 
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, constants.UpdatePaymentSettingsError)
+	}
 }
 func (h *Handler) updateSecuritySettings(c *gin.Context) {
+	var s entities.SecuritySettings
+
+	if err := c.BindJSON(&s); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, constants.BadRequest)
+	}
+	// TODO - Here
+	err := h.services.Settings.UpdateSecuritySettings(&s)
+
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, constants.UpdateSecuritySettingsError)
+	}
+}
+func (h *Handler) updatePrivacySettings(c *gin.Context) {
 
 }
--- pkg/repository/settings.go
+++ pkg/repository/settings.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/davitdarsalia/LendAppBackend/entities"
+
 func (r *SettingsPostgres) GetProfileSettings() {
 	//TODO implement me
 }
@@ -20,18 +22,18 @@ func (r *SettingsPostgres) GetSecuritySettings() {
 	//TODO implement me
 }
 
-func (r *SettingsPostgres) UpdateNotificationSettings() {
-	//TODO implement me
+func (r *SettingsPostgres) UpdateNotificationSettings(userID *int, s *entities.NotificationSettings) error {
+	return nil
 }
 
-func (r *SettingsPostgres) UpdatePaymentOptions() {
-	//TODO implement me
+func (r *SettingsPostgres) UpdatePaymentSettings(userID *int, s *entities.PaymentSettings) error {
+	return nil
 }
 
-func (r *SettingsPostgres) UpdatePrivacySettings() {
-	//TODO implement me
+func (r *SettingsPostgres) UpdateSecuritySettings(userID *int, s *entities.SecuritySettings) error {
+	return nil
 }
 
-func (r *SettingsPostgres) UpdateSecuritySettings() {
-	//TODO implement me
+func (r *SettingsPostgres) UpdatePrivacySettings() error {
+	return nil
 }
