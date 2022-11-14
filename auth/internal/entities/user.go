0ba45a3fa372b177897c43dd02d1c51b2623c723
Davit Darsalia <darsalia.david1998@gmail.com>
2022-11-15 02:04:42 +0400

    edit: UserTypes Fixed

--- auth/internal/entities/user.go
+++ auth/internal/entities/user.go
@@ -1,15 +1,24 @@
 package entities
 
+import "github.com/dgrijalva/jwt-go"
+
 type User struct {
-	UserID    uintptr `json:"user_id"`
-	UserName  uintptr `json:"username"`
-	UserRole  uintptr `json:"user_role"`
-	Password  uintptr `json:"password"`
-	FirstName uintptr `json:"first_name"`
-	LastName  uintptr `json:"last_name"`
-	Country   uintptr `json:"country"`
-	Email     uintptr `json:"email"`
-	City      uintptr `json:"city"`
-	CreatedAt uintptr `json:"createdAt"`
-	IpAddress uintptr `json:"ip_address"`
+	UserName  string `json:"username" binding:"required"`
+	UserRole  string `json:"user_role"  binding:"required"`
+	Password  string `json:"password"  binding:"required"`
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Country   string `json:"country" binding:"required"`
+	Email     string `json:"email" binding:"required"`
+	Gender    string `json:"gender" binding:"required"`
+	City      string `json:"city" binding:"required"`
+	CreatedAt string `json:"createdat"`
+	IpAddress string `json:"ip_address"`
+}
+
+type AccessToken struct {
+	jwt.StandardClaims
+	UserID string `json:"user_id"`
+	Role   string `json:"role"`
+	Ip     string `json:"ip_address"`
 }
