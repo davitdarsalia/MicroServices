e31fcbea9015bbf0849ae04848e303d9f16eae24
davitdarsalia1998 <darsalia.david1998@gmail.com>
2023-02-10 16:16:34 +0400

    feat: fix

--- email/internal/responses/responseStrings.go
+++
@@ -1,5 +0,0 @@
-package responses
-
-const CreateUserSuccessMessage = `[Create User Handler] - User Created Successfully`
-const LoggedInUserSuccessMessage = `[Log In User Handler] - User Logged In Successfully`
-const RecoveredPasswordSuccessMessage = `[Recovered Password Handler] - Password Recovered Successfully`
