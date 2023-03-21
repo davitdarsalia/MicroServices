26f8d7683f9af045abd072ae20a48733b8408ca3
davitdarsalia1998 <darsalia.david1998@gmail.com>
2023-03-21 14:11:14 +0400

    feat: Colored output custom middleware defined

--- auth/pkg/handler/middlewares.go
+++ auth/pkg/handler/middlewares.go
@@ -1,9 +1,108 @@
 package handler
 
 import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"github.com/gin-gonic/gin"
+	"sync"
+	"time"
 )
 
-func (h *Handler) authCheck(c *gin.Context) {
+import (
+	"fmt"
+)
 
+const (
+	brightGreen  = "\033[38;5;10m"
+	brightYellow = "\033[38;5;11m"
+	brightBlue   = "\033[38;5;12m"
+	brightOrange = "[38;5;208m"
+	brightPink   = "[38;5;207m"
+	reset        = "[0m"
+
+	reqIdLength = 5
+
+	separator = "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+)
+
+var counter uintptr = 1
+var logPool = sync.Pool{
+	New: func() interface{} {
+		return make([]byte, 0, 256)
+	},
+}
+
+type logMessage struct {
+	message []byte
+}
+
+func CustomLogger() gin.HandlerFunc {
+	logChan := make(chan *logMessage, 100)
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go loggerWorker(logChan, &wg)
+	shutdown := make(chan struct{})
+	var once sync.Once
+
+	go func() {
+		<-shutdown
+		close(logChan)
+		wg.Wait()
+	}()
+
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(c.Request.Context(), 5*time.Second)
+		defer cancel()
+
+		c.Request = c.Request.WithContext(ctx)
+
+		select {
+		case <-c.Done():
+			once.Do(func() {
+				close(shutdown)
+			})
+		default:
+			start := time.Now()
+
+			c.Next()
+
+			buf := logPool.Get().([]byte)
+			buf = append(buf, separator...)
+			buf = append(buf, fmt.Sprintf("\n%s \n[Request %sN%d%s - %sID %s%s] [%sUnauthorized User%s] -> Path: [%s%s%s] -> Date [%s%s%s] -> Latency: [%s%d%s Nano Seconds] -> StatusCode: [%s%d%s] -> [%s%f KB%s]\n",
+				brightYellow, brightOrange, counter, reset,
+				brightYellow,
+				generateShortUUID(), brightYellow, brightPink, reset, brightGreen, c.Request.URL.Path, reset,
+				brightBlue, time.Now().Format("2 January 2006 : 15:04:05:00"), reset,
+				brightPink, time.Since(start).Milliseconds(), reset, brightOrange, c.Writer.Status(), reset,
+				brightPink, float64(c.Writer.Size())/1024, reset,
+			)...)
+
+			logChan <- &logMessage{message: buf}
+			counter++
+		}
+	}
+}
+
+func loggerWorker(logChan <-chan *logMessage, wg *sync.WaitGroup) {
+	var buf bytes.Buffer
+	defer wg.Done()
+
+	for msg := range logChan {
+		buf.Reset()
+		buf.Write(msg.message)
+		fmt.Println(buf.String())
+		logPool.Put(msg.message[:0])
+	}
+}
+
+func generateShortUUID() string {
+	randBytes := make([]byte, reqIdLength)
+	_, err := rand.Read(randBytes)
+	if err != nil {
+		return ""
+	}
+	return hex.EncodeToString(randBytes)
 }
