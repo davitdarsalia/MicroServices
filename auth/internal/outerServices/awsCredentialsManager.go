9abaad9ad998e83d960519a56c12db9733220931
davitdarsalia1998 <darsalia.david1998@gmail.com>
2023-03-21 14:09:28 +0400

    feat: Aws secrets manager defined + injected in service + repository

--- auth/internal/outerServices/awsCredentialsManager.go
+++ auth/internal/outerServices/awsCredentialsManager.go
@@ -1,41 +1,55 @@
 package outerServices
 
 import (
+	"auth/internal/entities"
 	"context"
-	"log"
-
+	"encoding/json"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"log"
+	"os"
 )
 
-func GetAWSCredentials() string {
-	secretName := "ConfigsSecrets"
-	region := "eu-west-2"
+func GetSecret() (entities.AWSCredentials, error) {
+	var c entities.AWSCredentials
 
-	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	secretName := os.Getenv("SECRET_NAME")
+	region := os.Getenv("REGION")
+
+	cfg, err := config.LoadDefaultConfig(
+		context.TODO(),
+		config.WithRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(
+			os.Getenv("ACCESS_KEY_ID"),
+			os.Getenv("SECRET_ACCESS_KEY"),
+			"",
+		)),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Create Secrets Manager client
-	svc := secretsmanager.NewFromConfig(config)
+	svc := secretsmanager.NewFromConfig(cfg)
 
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId:     aws.String(secretName),
-		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+		VersionStage: aws.String(os.Getenv("VERSION_STAGE")),
 	}
 
 	result, err := svc.GetSecretValue(context.TODO(), input)
 	if err != nil {
-		// For a list of exceptions thrown, see
-		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
 		log.Fatal(err.Error())
 	}
 
-	// Decrypts secret using the associated KMS key.
-	var secretString string = *result.SecretString
+	var secretString = *result.SecretString
 
-	return secretString
+	err = json.Unmarshal([]byte(secretString), &c)
 
+	if err != nil {
+		return entities.AWSCredentials{}, err
+	}
+
+	return c, nil
 }
