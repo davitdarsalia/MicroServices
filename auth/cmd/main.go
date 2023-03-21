a5c9a3ba8e43b72211986b69945df414f03b3269
davitdarsalia1998 <darsalia.david1998@gmail.com>
2023-03-21 14:10:55 +0400

    feat: Graceful shutdown for main func

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 // @title Authentication Server
@@ -40,24 +42,34 @@ func main() {
 		PrettyPrint:       true,
 	})
 
-	conn, err := pgxpool.Connect(context.Background(), os.Getenv("DB_CONN_STRING"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	secrets, err := outerServices.GetSecret()
 	if err != nil {
 		logrus.Fatalf("Failed to create database: %v", err.Error())
 	}
 
+	conn, err := pgxpool.Connect(context.Background(), secrets.DBConnString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	v := validator.New()
-	mq := outerServices.MqConnection()
-	repos := repository.New(conn)
-	services := service.New(repos, mq, v)
+	mq := outerServices.MqConnection(&secrets.RabbitMQConn)
+	repos := repository.New(conn, &secrets)
+	services := service.New(repos, mq, v, &secrets)
 	h := handler.New(services)
 	s := new(entities.Server)
 
-	if err := s.Run(os.Getenv("AUTH_SERVER_PORT"), h.DefineRoutes()); err != nil {
-		logrus.Fatalf("Error occurred while initializing server: %s", err.Error())
-	}
-	// TODO - Close rabbitMQ connection on graceful shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := s.Run(secrets.ServerPort, h.DefineRoutes()); err != nil {
+			logrus.Fatalf("Error occurred while initializing server: %s", err.Error())
+		}
+	}()
+
+	<-signalChan
+
+	mq.Close()
+	conn.Close()
 }
