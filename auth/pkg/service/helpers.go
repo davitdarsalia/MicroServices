521421db474276180af8c8066b5d11bfb875a0fc
davitdarsalia1998 <darsalia.david1998@gmail.com>
2023-03-22 14:56:53 +0400

    feat: OTP generator defined

--- auth/pkg/service/helpers.go
+++ auth/pkg/service/helpers.go
@@ -8,7 +8,7 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/argon2"
-	_ "golang.org/x/crypto/bcrypt"
+	"math/big"
 	"net"
 	"os"
 	"regexp"
@@ -17,6 +17,34 @@ import (
 	"time"
 	"unicode/utf8"
 )
+
+func otp() (string, error) {
+	min := big.NewInt(100_000_000_000)
+	max := big.NewInt(999_999_999_999)
+
+	// Calculate the range
+	rangeInt := big.NewInt(0).Sub(max, min)
+	rangeInt.Add(rangeInt, big.NewInt(1))
+
+	// Generate a random number within the range
+	randomInt, err := rand.Int(rand.Reader, rangeInt)
+	if err != nil {
+		return "", err
+	}
+
+	// Add the minimum value to get a number within the desired range
+	randomInt.Add(randomInt, min)
+
+	// Convert the random number to a formatted string
+	otp := fmt.Sprintf("%03d %03d %03d %03d",
+		randomInt.Uint64()/1_000_000_000,
+		(randomInt.Uint64()%1_000_000_000)/1_000_000,
+		(randomInt.Uint64()%1_000_000)/1_000,
+		randomInt.Uint64()%1_000,
+	)
+
+	return otp, nil
+}
 
 func generateValidationStruct(e error) error {
 	fieldNames := make([]string, 0, 0)
@@ -101,7 +129,7 @@ func validateToken(tokenString string, pKey []byte) (bool, error) {
 }
 
 func checkUUID(uuid string) bool {
-	condition := regexp.MustCompile(`(?i)^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`)
+	condition := regexp.MustCompile(`^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`)
 
 	return condition.MatchString(uuid)
 }
